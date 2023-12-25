c10c1779c9b8f9b207fdc63a44bfaff9e75c228a
ociddoci <68173930+ociddoci@users.noreply.github.com>
2023-12-25 12:11:54 +0800

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.4.18
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://www.instagram.com/ociddoci/">ociddoci</a>.</strong> All rights
+    reserved.
+</footer>
